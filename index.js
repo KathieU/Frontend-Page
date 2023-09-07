45608d15b21a7140aed2133d2d67cd42d7a55706
Catherine <kathrynepatrick@gmail.com>
2023-09-07 15:25:28 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// Function to update the current day of the week
+function updateCurrentDayOfTheWeek() {
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const currentDate = new Date();
+  const dayOfWeek = daysOfWeek[currentDate.getUTCDay()];
+  document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent =
+    dayOfWeek;
+}
+
+// Function to update the current UTC time
+
+function updateCurrentUTCTime() {
+  const currentMillis = new Date().getTime();
+  document.querySelector('[data-testid="currentUTCTime"]').textContent =
+    currentMillis;
+}
+
+// Initial update of real-time data
+updateCurrentDayOfTheWeek();
+updateCurrentUTCTime();
+
+// Update real-time data every second
+setInterval(function () {
+  updateCurrentDayOfTheWeek();
+  updateCurrentUTCTime();
+}, 1000);
